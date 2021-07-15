0821e61972b8d394f3337d20dda9fab5095bef87
rodneyviana <27776514+rodneyviana@users.noreply.github.com>
2021-07-15 18:56:10 -0500

    Added SQL and Functions

---
+++ BadWebApp/FunctionAccess.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="Access Function" Language="C#" AutoEventWireup="true" CodeBehind="FunctionAccess.aspx.cs" MasterPageFile="~/Site.Master" Inherits="BadWebApp.FunctionAccess" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>This will call an Azure Function.</h3>
+    <br />
+    <p>Enter the optional category filter (e.g: bike). Having ";" or "'" in the filter causes an error</p>
+    <br />
+    <span style="text-align: center; margin: auto; display: table">
+    <asp:Label runat="server">Category Filter: </asp:Label>
+    &nbsp;&nbsp;
+    <asp:TextBox runat="server" ID="Filter" Text=""></asp:TextBox>
+    &nbsp;<asp:Button runat="server" ID="Button1" Text="Update" BackColor="#66CCFF" Font-Bold="True" ForeColor="White" OnClick="Button1_Click" />
+        </span>
+    <br />
+    <asp:Label runat="server" BackColor="Red" ForeColor="White" Font-Bold="true" ID="ErrorLabel" Text=""></asp:Label>
+<asp:GridView ID="GridView1" runat="server" HorizontalAlign="Center" style="margin: 8px;">
+    <RowStyle BackColor="White" Font-Italic="True" />
+    <EmptyDataRowStyle BackColor="Yellow" />
+    <PagerStyle BackColor="#FFC0C0" Font-Italic="True" Font-Underline="True" />                
+    <SelectedRowStyle BackColor="#00C000" Font-Bold="True" Font-Italic="True" />
+    <EditRowStyle BackColor="#0000C0" />
+    <AlternatingRowStyle BackColor="Wheat" BorderColor="Green" BorderStyle="Dashed"   BorderWidth="1px" Font-Bold="True" />
+    <FooterStyle BackColor="#00C0C0" />
+    <HeaderStyle BackColor="PaleTurquoise" Font-Bold="True" Font-Italic="True" Font-Underline="True" />  
+</asp:GridView>
+<asp:ObjectDataSource ID="FunctionDataSource" runat="server"></asp:ObjectDataSource>
+    
+</asp:Content>
---
+++ BadWebApp/SqlAccess.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace BadWebApp
+{
+    public partial class SqlAccess : System.Web.UI.Page
+    {
+        public ProductCategories products;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlProductCategories.Filter = Filter.Text;
+            SqlProductCategories.Retrieve();
+            GridView1.DataSource = SqlProductCategories.Categories;
+            GridView1.DataBind();
+            if(SqlProductCategories.ErrorMessage != null)
+            {
+                ErrorLabel.Text = "Error: "+SqlProductCategories.ErrorMessage;
+            } else
+            {
+                ErrorLabel.Text = "";
+            }
+        }
+    }
+
+
+    public class SqlProductCategories
+    {
+        const string error = "Category filter cannot contain ' or ;";
+        public static List<ProductCategory> Retrieve()
+        {
+            string conn = Environment.GetEnvironmentVariable("AzureSqlConnection");
+
+            if (null != Filter && (Filter.Contains("'") || Filter.Contains(";")))
+            {
+                Categories = new List<ProductCategory>();
+
+                ErrorMessage = error;
+                return Categories;
+            }
+
+            using (SqlConnection connection = new SqlConnection(conn))
+            {
+                string query = null;
+                if (String.IsNullOrEmpty(Filter))
+                {
+                    query = "SELECT CAST((SELECT * FROM SalesLT.vGetAllCategories FOR JSON PATH) AS VARCHAR(MAX))";
+                }
+                else
+                {
+                    query = "SELECT CAST((SELECT * FROM SalesLT.vGetAllCategories WHERE ProductCategoryName LIKE @Cat FOR JSON PATH) AS VARCHAR(MAX))";
+                }
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    if (query.Contains("@Cat"))
+                    {
+                        cmd.Parameters.AddWithValue("@Cat", "%" + Filter + "%");
+                    }
+                    connection.Open();
+                    string result = null;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            result = reader[0] as string;
+                            break;
+                        }
+                    }
+                    Categories = JsonConvert.DeserializeObject<List<ProductCategory>>(result);
+                    ErrorMessage = null;
+                    return Categories;
+                }
+            }
+
+        }
+
+        public static string ErrorMessage = null;
+
+        public static string Filter = null;
+        public static List<ProductCategory> Categories { get; set; }
+    }
+
+}
